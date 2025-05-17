cb15f73201a282592bca3b712f64d8d4e63bcfd2
Aleksandar Zorkic <aleksandarzorkic@gmail.com>
2025-05-17 08:53:40 +0200

    Dodata funkcionalnost brisanja knjige iz tabele

---
+++ library/library/app/bookReview/bookReview.js
@@ -0,0 +1,89 @@
+"use strict"
+
+class Knjige {
+    constructor(id, naziv, datum, slika, opis, recenzija) {
+        this.id = id
+        this.naziv = naziv
+        this.datum = datum
+        this.slika = slika
+        this.opis = opis
+        this.recenzija = recenzija
+    }
+}
+
+let knjige = []
+
+function initializeBooks() {
+    
+    const knjigeString = localStorage.getItem('knjige')
+
+    if (knjigeString) {
+        knjige = JSON.parse(knjigeString).map (k => 
+            new Knjige (k.id, k.naziv, k.datum, k.slika, k.opis, k.recenzija))
+    }
+    else {
+        knjige = [
+            new Knjige (
+                1,
+                "Most na Zepi",
+                "01.10.1925.",
+                "https://www.scribd.com/doc/142854212/Most-na-%C5%BDepi-Ivo-Andri%C4%87",
+                "Pripovetka o izgradnji kamenog mosta na nabujaloj reci Žepi.",
+                4.17
+            ),
+
+            new Knjige (
+                2,
+                "Proces",
+                "26.04.1925.",
+                "https://files.libcom.org/files/The%20Trial%20-%20Franz%20Kafka.pdf ",
+                "Roman o Josef K., bankarskom činovniku koji je iznenada uhapšen i suđen u neuhvatljivom, apsurdnom pravnom sistemu, pri čemu mu priroda optužbe ostaje nepoznata.",            
+                3.95
+            ),               
+            
+            new Knjige (
+                3,
+                "1984",
+                "08.06.1949.",
+                "https://images.app.goo.gl/jPfKNR1GSDA1Tf1T7",
+                "Distopijski roman o totalitarnom režimu koji nadgleda svaki aspekt ljudskog života, prateći život Vinstona Smita koji pokušava da zadrži svoju slobodu mišljenja.",
+                4.19
+            )
+        ]
+        localStorage.setItem("knjige", JSON.stringify(knjige))
+    }
+    createBooksRow(knjige)
+}
+
+function createBooksRow (knjige) {
+    let table = document.querySelector("#book-list")
+    table.innerHTML = ''
+
+    for (let i = 0; i < knjige.length; i++) {
+        let tr = document.createElement("tr")
+
+        let id = document.createElement("td")
+        id.textContent = i + 1
+
+        let naziv = document.createElement("td")
+        naziv.textContent = knjige[i].naziv
+
+        let obrisi = document.createElement("td")
+        let obrisiBtn = document.createElement("button")
+        obrisiBtn.textContent = "Obrisi"
+        obrisiBtn.addEventListener('click', () => deleteBook(i))
+
+        obrisi.appendChild(obrisiBtn)
+        tr.append(id, naziv, obrisi)
+        table.appendChild(tr)
+        
+    }
+}
+
+function deleteBook(i) {
+    knjige.splice(i, 1)
+    localStorage.setItem('knjige', JSON.stringify(knjige))
+    createBooksRow(knjige)
+}
+
+document.addEventListener('DOMContentLoaded', initializeBooks)
